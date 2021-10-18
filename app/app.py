4fa3eef881f9c582f549baff4d925239633ab2f3
Miguel Arias <arias.miguelm@gmail.com>
2021-10-18 14:11:52 -0500

    html basico de login y main mas carpetas raices

--- app/app.py
+++ app/app.py
@@ -1,3 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/main')
+def main ():
+    return render_template('main/main.html')
+
+@app.route('/login')
+def login ():
+    return render_template('login/login.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios/usuarios.html')
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 8000)
